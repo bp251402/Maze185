4348448b77363168acca06f825e83f401d42d325
bp251402 <bp251402@muhlenberg.edu>
2016-09-07 20:41:20 -0400

    testing for turtle

--- mazetest.py
+++ mazetest.py
@@ -12,7 +12,10 @@ class testMaze(unittest.TestCase):
         pass
     
     def testScreenExists(self):
-        assert type(self.m) == Maze
+        assert type(self.m.s) == turtle._Screen
+
+    def testTurtleExists(self):
+        assert type(self.m.t) == turtle._Turtle
 
 if __name__=="__main__":
     unittest.main(exit=False)
